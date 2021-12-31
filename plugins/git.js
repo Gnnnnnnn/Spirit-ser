58af4b3092715a7a2822ab950271e58f097923ee
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-31 23:45:03 +0530

    spirit ser💝

--- plugins/git.js
+++ plugins/git.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://i.imgur.com/qdkhgpq.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*SPIRIT SER bot created by SPIRIT *
-*Creator number : wa.me/18052196558?text=Hi%20splrlt%20ser.%20*
+*Creator number : wa.me/18052196558?text=Hi%20spirit%20.%20*
 
 *subscribe  my channel  : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA*
 
