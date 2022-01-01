6265cf67820a25a370bf4f57580ec2d601f44593
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2022-01-01 22:28:38 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,7 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/qdkhgpq.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/P47w0P3.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*SPIRIT SER bot created by SPIRIT *
 *Creator number : wa.me/18052196558?text=Hi%20spirit%20.%20*
